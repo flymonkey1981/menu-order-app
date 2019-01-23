2462709da48547e2332c8757bd761669ea30730f
charles.chen <charles.chen@iress.com>
2019-01-23 16:16:24 +1100

    scheduled commit

--- src/components/AddAdmin.js
+++ src/components/AddAdmin.js
@@ -5,6 +5,7 @@ import '../styles/components/addAdmin.scss';
 const propTypes = {
     addAdmin: PropTypes.func.isRequired
 };
+
 class AddAdmin extends Component {
 
     constructor() {
@@ -38,87 +39,113 @@ class AddAdmin extends Component {
         const {addAdmin, history} = this.props;
         const {form} = this.state;
         return (
-            <div className="add-admin">
-                <h1>Add Admin</h1>
-                <form action="" method="post" onSubmit={(event) => {
-                    event.preventDefault();
-                    addAdmin(form, history);
-                }}>
-                    <div className="form-group">
-                        <lable for="lastName">Last Name:</lable>
-                        <input className="form-control" type="text" name="lastName" className="form-control" value={form.lastName}
-                               onChange={this.onChange} placeholder="Last Name"/>
+            <div className="add-admin-wrapper">
+                <div className="add-admin-container">
+                    <h1>Add Admin</h1>
+                    <div className="add-admin-top">
+                        <form action="" method="post" onSubmit={(event) => {
+                            event.preventDefault();
+                            addAdmin(form, history);
+                        }}>
+                            <div className="form-group">
+                                <lable for="lastName">Last Name:</lable>
+                                <input className="form-control" type="text" name="lastName" className="form-control text"
+                                       value={form.lastName}
+                                       onChange={this.onChange} placeholder="Last Name" required/>
+                            </div>
+
+                            <div className="form-group">
+                                First Name:
+                                <input className="form-control" type="text" name="firstName" className="form-control text"
+                                       value={form.firstName}
+                                       onChange={this.onChange} placeholder="First Name" required/>
+                            </div>
+
+                            <div className="form-group">
+                                UserName:
+                                <input className="form-control" type="text" name="userName" className="form-control text"
+                                       value={form.userName}
+                                       onChange={this.onChange} placeholder="Username" required/>
+                            </div>
+
+                            <div className="form-group">
+                                Phone Number:
+                                <input className="form-control" type="text" name="phoneNumber" className="form-control text"
+                                       value={form.phoneNumber}
+                                       onChange={this.onChange} placeholder="phoneNumber" required/>
+                            </div>
+
+                            <div className="form-group">
+                                Password:
+                                <input className="form-control" type="text" name="password" className="form-control text"
+                                       value={form.password}
+                                       onChange={this.onChange} placeholder="password" required/>
+                            </div>
+
+                            <div className="form-group">
+                                Address Line 1:
+                                <input className="form-control" type="text" name="addressLine1" className="form-control text"
+                                       value={form.addressLine1}
+                                       onChange={this.onChange} placeholder="addressLine1" required/>
+                            </div>
+
+                            <div className="form-group">
+                                Address Line 2:
+                                <input className="form-control" type="text" name="addressLine2" className="form-control text"
+                                       value={form.addressLine2}
+                                       onChange={this.onChange} placeholder="addressLine2" required/>
+                            </div>
+
+                            <div className="form-group">
+                                Suburb:
+                                <input className="form-control" type="text" name="suburb" className="form-control text"
+                                       value={form.suburb}
+                                       onChange={this.onChange} placeholder="suburb" required/>
+                            </div>
+
+                            <div className="form-group">
+                                State:
+                                <input className="form-control" type="text" name="state" className="form-control text"
+                                       value={form.state}
+                                       onChange={this.onChange} placeholder="state" required/>
+                            </div>
+
+                            <div className="form-group">
+                                Country:
+                                <input className="form-control" type="text" name="country" className="form-control text"
+                                       value={form.country}
+                                       onChange={this.onChange} placeholder="country" required/>
+                            </div>
+
+                            <div className="form-group">
+                                Post Code:
+                                <input className="form-control" type="text" name="postCode" className="form-control text"
+                                       value={form.postCode}
+                                       onChange={this.onChange} placeholder="postCode" required/>
+                            </div>
+
+
+                            <div className="form-group">
+                                <input type="submit" name="btn" value="Add"
+                                       className="btn btn-outline-danger float-right login_btn"/>
+                            </div>
+
+
+                        </form>
                     </div>
-
-                    <div className="form-group">
-                        First Name:
-                        <input className="form-control" type="text" name="firstName" className="form-control" value={form.firstName}
-                               onChange={this.onChange} placeholder="First Name"/>
-                    </div>
-
-                    <div className="form-group">
-                        UserName:
-                        <input className="form-control" type="text" name="userName" className="form-control" value={form.userName}
-                               onChange={this.onChange} placeholder="Username"/>
-                    </div>
-
-                    <div className="form-group">
-                        Phone Number:
-                        <input className="form-control" type="text" name="phoneNumber" className="form-control" value={form.phoneNumber}
-                               onChange={this.onChange} placeholder="phoneNumber"/>
-                    </div>
-
-                    <div className="form-group">
-                        Password:
-                        <input className="form-control" type="text" name="password" className="form-control" value={form.password}
-                               onChange={this.onChange} placeholder="password"/>
-                    </div>
-
-                    <div className="form-group">
-                        Address Line 1:
-                        <input className="form-control" type="text" name="addressLine1" className="form-control" value={form.addressLine1}
-                               onChange={this.onChange} placeholder="addressLine1"/>
-                    </div>
-
-                    <div className="form-group">
-                        Address Line 2:
-                        <input className="form-control" type="text" name="addressLine2" className="form-control" value={form.addressLine2}
-                               onChange={this.onChange} placeholder="addressLine2"/>
-                    </div>
-
-                    <div className="form-group">
-                        Suburb:
-                        <input className="form-control" type="text" name="suburb" className="form-control" value={form.suburb}
-                               onChange={this.onChange} placeholder="suburb"/>
-                    </div>
-
-                    <div className="form-group">
-                        State:
-                        <input className="form-control" type="text" name="state" className="form-control" value={form.state}
-                               onChange={this.onChange} placeholder="state"/>
-                    </div>
-
-                    <div className="form-group">
-                        Country:
-                        <input className="form-control" type="text" name="country" className="form-control" value={form.country}
-                               onChange={this.onChange} placeholder="country"/>
-                    </div>
-
-                    <div className="form-group">
-                        Post Code:
-                        <input className="form-control" type="text" name="postCode" className="form-control" value={form.postCode}
-                               onChange={this.onChange} placeholder="postCode"/>
-                    </div>
-
-
-
-                    <div className="form-group">
-                        <input type="submit" name="btn" value="Add"
-                               className="btn btn-outline-danger float-right login_btn"/>
-                    </div>
-
-
-                </form>
+                </div>
+                <ul className="colorlib-bubbles">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
 
             </div>
         );
--- src/components/Login.js
+++ src/components/Login.js
@@ -30,41 +30,44 @@ class Login extends Component {
         const {login, history} = this.props;
         const {form} = this.state;
         return (
-            <div className="container">
-                <div className="card card-login mx-auto text-center bg-dark">
-                    <div className="card-header mx-auto bg-dark">
+            <div className="login">
+                <div className="container">
+                    <div className="card card-login mx-auto text-center bg-dark">
+                        <div className="card-header mx-auto bg-dark">
                         <span> <img src="https://amar.vote/assets/img/amarVotebd.png" className="w-75"
                                     alt="Logo"/> </span><br/>
-                        <span className="logo_title mt-5"> Login Dashboard </span>
-                    </div>
-                    <div className="card-body">
-                        <form action="/products" method="post" onSubmit={(event) => {
-                            event.preventDefault();
-                            login(form, history);
-                        }}>
-                            <div className="input-group form-group">
-                                <div className="input-group-prepend">
-                                    <span className="input-group-text"><i className="fas fa-user"></i></span>
+                            <span className="logo_title mt-5"> Login Dashboard </span>
+                        </div>
+                        <div className="card-body">
+                            <form action="/products" method="post" onSubmit={(event) => {
+                                event.preventDefault();
+                                login(form, history);
+                            }}>
+                                <div className="input-group form-group">
+                                    <div className="input-group-prepend">
+                                        <span className="input-group-text"><i className="fas fa-user"></i></span>
+                                    </div>
+                                    <input type="text" name="email" className="form-control" value={form.email}
+                                           onChange={this.onChange} placeholder="Username"/>
                                 </div>
-                                <input type="text" name="email" className="form-control" value={form.email}
-                                       onChange={this.onChange} placeholder="Username"/>
-                            </div>
 
-                            <div className="input-group form-group">
-                                <div className="input-group-prepend">
-                                    <span className="input-group-text"><i className="fas fa-key"></i></span>
+                                <div className="input-group form-group">
+                                    <div className="input-group-prepend">
+                                        <span className="input-group-text"><i className="fas fa-key"></i></span>
+                                    </div>
+                                    <input type="password" name="password" className="form-control"
+                                           value={form.password}
+                                           onChange={this.onChange} placeholder="Password"/>
                                 </div>
-                                <input type="password" name="password" className="form-control" value={form.password}
-                                       onChange={this.onChange} placeholder="Password"/>
-                            </div>
 
-                            <div className="form-group">
-                                <input type="submit" name="btn" value="Login"
-                                       className="btn btn-outline-danger float-right login_btn"/>
-                            </div>
+                                <div className="form-group">
+                                    <input type="submit" name="btn" value="Login"
+                                           className="btn btn-outline-danger float-right login_btn"/>
+                                </div>
 
 
-                        </form>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
